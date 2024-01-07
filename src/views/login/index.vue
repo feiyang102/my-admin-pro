0da113ba9d4080b37a3eb6c1f3812950274d55fb
feiyang <feiyang102@outlook.com>
2024-01-07 21:49:12 +0800

    Store Pinia 完善

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,10 @@
 
 <script lang="ts" setup>
 // import { reactive, toRefs, ref, onBeforeMount, onMounted } from "vue";
+import { useUserStore } from '@/store/user';
+
+const userStore = useUserStore();
+console.log(`userStore: ${JSON.stringify(userStore)}`);
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
