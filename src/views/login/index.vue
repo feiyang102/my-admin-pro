9950a642271ae0f1bb19e89c88108e5d20650047
feiyang <feiyang102@outlook.com>
2024-01-07 21:40:59 +0800

    封装路由,axios

---
+++ src/views/login/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>login</div>
+</template>
+
+<script lang="ts" setup>
+// import { reactive, toRefs, ref, onBeforeMount, onMounted } from "vue";
+</script>
+
+<style scoped>
+</style>
