97f4da2c79a8fd1d879534f8410b08efe0d7a1ed
feiyang <feiyang102@outlook.com>
2024-01-20 03:59:35 +0800

    refactor: 权限修改使用store

--- src/components/AuthTree.vue
+++ src/components/AuthTree.vue
@@ -15,10 +15,11 @@
 
 <script lang="ts" setup>
 import { ref, onMounted, defineProps, defineExpose } from "vue";
-import { authTree } from "@/api/auth";
+import { useAuthStore } from "@/store/auth";
+const authStore = useAuthStore();
+
 const props = defineProps(["roles", "name"]);
 const tree = ref<any>(null);
-console.log(props.roles);
 
 const defaultProps = {
   children: "roleList",
@@ -28,15 +29,14 @@ const defaultProps = {
 const data = ref([]);
 
 const getCheckedKeys = () => {
-  console.log(111);
   return tree.value?.getCheckedKeys();
 };
 
 onMounted(async () => {
-  const res: any = await authTree().catch((err) => {
+  const res: any = await authStore.getAuthTree().catch((err) => {
     console.log(err);
   });
-  data.value = res.data;
+  data.value = res;
 });
 
 defineExpose({
---
+++ src/store/auth.ts
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia";
+import { authTree } from "@/api/auth";
+
+export const useAuthStore = defineStore("auth", () => {
+  const authList = ref<any>([]);
+  async function getAuthTree() {
+    if (authList.value.length == 0) {
+      // 获取权限列表的逻辑
+      const res = await authTree();
+      authList.value = res.data;
+      return res.data;
+    } else {
+      return authList.value;
+    }
+  }
+
+  return {
+    authList,
+    getAuthTree,
+  };
+});
