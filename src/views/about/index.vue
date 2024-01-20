4d40afeb332f1f92321355b8352bc213257a13d2
feiyang <feiyang102@outlook.com>
2024-01-21 02:16:15 +0800

    feat: less全局变量预处理additionalData

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>about</div>
+  <div class="wrap-about">about</div>
 </template>
 
 <script lang="ts" setup>
 // import { reactive, toRefs, ref, onBeforeMount, onMounted } from "vue";
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+// @import "@/styles/variable.less";
+.wrap-about {
+  color: @color;
+  background: @bg;
+}
+</style>
