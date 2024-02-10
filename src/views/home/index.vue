eec5e17297d6dfd1aa5bab988c6ce1d14c492ae4
feiyang102 <feiyang102@outlook.com>
2024-02-10 23:20:01 +0800

    temp: nextTick test

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>home</div>
+  <div>home {{ homeValue }}</div>
 </template>
 
 <script lang="ts" setup>
-// import { reactive, toRefs, ref, onBeforeMount, onMounted } from "vue";
+import { nextTick, ref } from "vue";
+let homeValue = ref(0);
+setTimeout(() => {
+  console.log(1111);
+
+  homeValue.value = 11111;
+}, 0);
+
+setTimeout(() => {
+  console.log(22222);
+  homeValue.value = 22222;
+}, 1000);
+
+Promise.resolve().then(() => {
+  console.log(33333);
+  homeValue.value = 33333;
+});
+nextTick(() => {
+  console.log(4444);
+  homeValue.value = 4444;
+});
 </script>
 
 <style scoped></style>
